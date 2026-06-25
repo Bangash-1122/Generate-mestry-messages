dde0d8fda43d87d299bf8c3aebf17333ad48c2a3
Bangash1122 <ubaidkhan0077800@gmail.in>
2026-06-24 21:09:50 -0400

    Initial commit

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "@/emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyUrl: string
+): Promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from: 'Mestry Message <onboarding@resend.dev>',
+            to: email,
+            subject: 'Mystery Message |  Verification Code',
+            react: VerificationEmail({username, otp: verifyUrl})
+        })
+        return {success: true, message: 'Verification email sent successfully'}
+    }catch(emailError){
+        console.error('Error sending verification email:', emailError)
+        return {success: false, message: 'Failed to send verification email'}
+    }
+}
