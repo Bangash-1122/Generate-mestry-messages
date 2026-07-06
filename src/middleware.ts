70654fd82651bf45c9b7a2d30cf2c76947178c45
Bangash1122 <ubaidkhan0077800@gmail.in>
2026-07-05 20:20:47 -0400

    Create a middleware and authprovider and changing for layout and option files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server' 
+
+
 export { default } from "next-auth/middleware"
 import { getToken } from "next-auth/jwt"
 
@@ -18,7 +20,10 @@ export async function middleware(request: NextRequest) {
     ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    return NextResponse.redirect(new URL('/home', request.url))
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+    return NextResponse.next()
 }
 
 export const config = {
