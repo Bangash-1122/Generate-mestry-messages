9786e157405860a24d53216a180067f364583c3b
Bangash1122 <ubaidkhan0077800@gmail.in>
2026-07-14 20:53:23 -0400

    feat: add sign-up form with username validation and API integration

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,59 +1,9 @@
-'use client'
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-import { useState, useEffect } from "react"
-import { useDebounceValue } from 'usehooks-ts'
-import { Toaster } from "@/components/ui/sonner"
-import axios , {AxiosError} from "axios";
-import { ApiResponse } from "@/types/apiResponse";
+import React from 'react'
 
 const page = () => {
-    const [username, setUsername] = useState('')
-    const [usernameMessage, setUsernameMessage] = useState('')
-    const [isCheckingUsername, setIsCheckingUsername] = useState(false)
-    const [isSubmitting, setIsSubmitting] = useState(false)
-
-    const debouncedUsername = useDebounceValue(username, 300)
-    const { toast } = useToast()
-    const router = useRouter();
-
-    // Zod implementation
-    const form = useForm<z.infer<typeof signUpSchema>>({
-        resolver: zodResolver(signUpSchema),
-        defaultValues: {
-            username: '',
-            email: '',
-            password: ''
-        }
-    })
-
-    useEffect(() => {
-        const checkUsernameUnique = async () => {
-            if(debouncedUsername){
-            setIsCheckingUsername(true)
-            setUsernameMessage('')
-            try {
-                const response = await axios.get(`/api/check-username?username=${debouncedUsername}`)
-                setUsernameMessage(response.data.message)
-            } catch (error) {
-                const axiosError = error as AxiosError<ApiResponse>;
-                setUsernameMessage(axiosError.response?.data.message || "Error while checking username");
-            }finally {
-                setIsCheckingUsername(false)
-            }
-            }
-        }
-        checkUsernameUnique()
-    }, [debouncedUsername])
-
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>page</div>
+  )
 }
 
 export default page
